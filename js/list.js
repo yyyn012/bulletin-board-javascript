9a05bb0f3993d19aef5c941a60ae7b8a7ff54fc8
yyyn012 <111561826+yyyn012@users.noreply.github.com>
2024-05-24 21:52:38 +0900

    pagination - data cutting

--- js/list.js
+++ js/list.js
@@ -51,95 +51,102 @@ const mouseOut = (event) => {
 const boardsObj = JSON.parse(localStorage.getItem(BOARDS));
 const tbody = document.querySelector("tbody");
 
-// for (let i = 0; i < boardsObj.length; i++) {
-//   tbody.innerHTML += template(i, boardsObj[i]);
-//   boardsObj[i].refresh = false;
-//   const refreshStr = JSON.stringify(boardsObj);
-//   localStorage.setItem(BOARDS, refreshStr);
-// }
-
 // pagination
+let totalPage = [];
 
-let totalPage = boardsObj.length;
+for (let i = 0; i < boardsObj.length; i++) {
+  totalPage.push(boardsObj[i]);
+}
+
+let pageLength = boardsObj.length - 1;
 let pageNum = 5;
 let blockNum = 5;
 let totalBlock = totalPage % 5 == 0 ? totalPage / 5 : totalPage / 5 + 1;
 let currentBlock = 1;
 
-let dataPrint = (block) => {
-  tbody.remove();
+let dataBlock = [];
 
-  let start = totalPage - pageNum * (block - 1);
-  for (let i = start; i >= 1 && i > start - pageNum; i--) {
-    tbody.innerHTML += template(i, boardsObj[i]);
-    boardsObj[i].refresh = false;
-    const refreshStr = JSON.stringify(boardsObj);
-    localStorage.setItem(BOARDS, refreshStr);
-  }
+const sliceBoardsStr = () => {
+  for (let i = 0; i <= pageLength; i += 5) {
+    data = totalPage.slice(i, i + 5);
 
-  for (let j = 1; j < boardsObj.length; j += 5) {
-    let temp;
-    temp = dataPrint(j).slice(j, j + 5);
-    console.log(temp[1]);
+    // template(i, boardsObj[i]);
+
+    for (let j = data.length - 1; j <= data.length; j--) {
+      tbody.innerHTML += template(j, boardsObj[j]);
+      boardsObj[j].refresh = false;
+      const refreshStr = JSON.stringify(boardsObj);
+      localStorage.setItem(BOARDS, refreshStr);
+    }
   }
 };
+
+console.log(sliceBoardsStr());
+
+// let dataPrint = (block) => {
+//   tbody.remove();
+
+//   let start = totalPage - pageNum * (block - 1);
+
+//   for (let i = start; i >= 1 && i > start - pageNum; i--) {
+//     tbody.innerHTML += template(i, boardsObj[i]);
+//     boardsObj[i].refresh = false;
+//     const refreshStr = JSON.stringify(boardsObj);
+//     localStorage.setItem(BOARDS, refreshStr);
+//   }
+// };
 
 // 게시글 데이터 출력하기
 
-// 블럭 출력하기
-// 매개변수 : 가장 앞에 오는 블럭
-function blockPrint(frontBlock) {
-  currentBlock = frontBlock;
-  const beforeBtn = document.querySelector(".before_move");
-  const nextBtn = document.querySelector(".next_move");
+// function blockPrint(frontBlock) {
+//   currentBlock = frontBlock;
+//   const beforeBtn = document.querySelector(".before_move");
+//   const nextBtn = document.querySelector(".next_move");
 
-  if (frontBlock <= 1) {
-    beforeBtn.style.visibility = "hidden";
-  } else {
-    beforeBtn.style.visibility = "visible";
-  }
+//   if (frontBlock <= 1) {
+//     beforeBtn.style.visibility = "hidden";
+//   } else {
+//     beforeBtn.style.visibility = "visible";
+//   }
 
-  // 다음으로 갈 블럭이 없으면
-  if (frontBlock + blockNum >= totalBlock) {
-    nextBtn.style.visibility = "hidden";
-  } else {
-    nextBtn.style.visibility = "visible";
-  }
+// if (frontBlock + blockNum >= totalBlock) {
+//   nextBtn.style.visibility = "hidden";
+// } else {
+//   nextBtn.style.visibility = "visible";
+// }
 
-  // 블럭을 추가할 공간
-  let blockBox = document.querySelector(".block");
-  // 기존 블럭 모두 삭제
-  blockBox.replaceChildren();
+// let blockBox = document.querySelector(".block");
+// blockBox.replaceChildren();
 
-  console.log("remove");
+// console.log("remove");
 
-  //front_block부터 total_block 또는 block_num까지 생성 및 추가
-  for (let i = frontBlock; i <= totalBlock && i < frontBlock + blockNum; i++) {
-    console.log("add element");
+//front_block부터 total_block 또는 block_num까지 생성 및 추가
+//   for (let i = frontBlock; i <= totalBlock && i < frontBlock + blockNum; i++) {
+//     console.log("add element");
 
-    // 버튼을 생성한다.s
-    let pageButton = document.createElement("button");
-    pageButton.textContent = i;
-    // 버튼을 클릭하면 게시글이 변경되는 이벤트 추가
-    pageButton.addEventListener("click", function (event) {
-      pagePrint(i);
-    });
-    // 블럭에 추가한다.
-    blockBox.appendChild(pageButton);
-  }
-}
+//     // 버튼을 생성한다.s
+//     let pageButton = document.createElement("button");
+//     pageButton.textContent = i;
+//     // 버튼을 클릭하면 게시글이 변경되는 이벤트 추가
+//     pageButton.addEventListener("click", function (event) {
+//       pagePrint(i);
+//     });
+//     // 블럭에 추가한다.
+//     blockBox.appendChild(pageButton);
+//   }
+// }
 
-function before() {
-  blockPrint(currentBlock - blockNum);
-  console.log("이전");
-}
+// function before() {
+//   blockPrint(currentBlock - blockNum);
+//   console.log("이전");
+// }
 
-function next() {
-  blockPrint(currentBlock + blockNum);
-  console.log("다음");
-}
-// 화면 로드 시 실행되는 이벤트
-window.onload = function () {
-  dataPrint(1);
-  blockPrint(1);
-};
+// function next() {
+//   blockPrint(currentBlock + blockNum);
+//   console.log("다음");
+// }
+// // 화면 로드 시 실행되는 이벤트
+// window.onload = function () {
+//   dataPrint(1);
+//   blockPrint(1);
+// };
