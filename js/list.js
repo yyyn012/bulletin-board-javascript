fea9a8d3a30868fa4a987b11893dceba7dd4a56b
yyyn012 <111561826+yyyn012@users.noreply.github.com>
2024-05-25 17:52:53 +0900

    pagination

--- js/list.js
+++ js/list.js
@@ -46,12 +46,12 @@ const mouseOut = (event) => {
   event.target.style.fontWeight = "500";
 };
 
-// template 반영
+// pagination
 
 const boardsObj = JSON.parse(localStorage.getItem(BOARDS));
 const tbody = document.querySelector("tbody");
 
-// pagination
+// 데이터 배열로 저장하기
 let totalPage = [];
 
 for (let i = 0; i < boardsObj.length; i++) {
@@ -66,87 +66,81 @@ let currentBlock = 1;
 
 let dataBlock = [];
 
-const sliceBoardsStr = () => {
-  for (let i = 0; i <= pageLength; i += 5) {
-    data = totalPage.slice(i, i + 5);
-
-    // template(i, boardsObj[i]);
-
-    for (let j = data.length - 1; j <= data.length; j--) {
-      tbody.innerHTML += template(j, boardsObj[j]);
-      boardsObj[j].refresh = false;
-      const refreshStr = JSON.stringify(boardsObj);
-      localStorage.setItem(BOARDS, refreshStr);
-    }
+// 저장된 데이터 5개씩 꺼내 출력하는 함수
+// const sliceBoardsStr = (block) => {
+//   let start = block * pageNum;
+//   console.log(start);
+//   for (let i = start; i < 0; --i) {
+//     data = totalPage.slice(i, 1);
+//     dataBlock.push(data);
+//     dataPrint(data);
+//     console.log(dataBlock);
+//   }
+//   tbody.remove();
+// };
+dataPrint = (data) => {
+  for (let j = data.length - 1; j <= data.length; j--) {
+    tbody.innerHTML += template(j, boardsObj[j]);
+    boardsObj[j].refresh = false;
+    const refreshStr = JSON.stringify(boardsObj);
+    localStorage.setItem(BOARDS, refreshStr);
   }
 };
 
-console.log(sliceBoardsStr());
-
-// let dataPrint = (block) => {
-//   tbody.remove();
-
-//   let start = totalPage - pageNum * (block - 1);
-
-//   for (let i = start; i >= 1 && i > start - pageNum; i--) {
-//     tbody.innerHTML += template(i, boardsObj[i]);
-//     boardsObj[i].refresh = false;
-//     const refreshStr = JSON.stringify(boardsObj);
-//     localStorage.setItem(BOARDS, refreshStr);
-//   }
-// };
+console.log(dataPrint(2));
 
 // 게시글 데이터 출력하기
 
-// function blockPrint(frontBlock) {
-//   currentBlock = frontBlock;
-//   const beforeBtn = document.querySelector(".before_move");
-//   const nextBtn = document.querySelector(".next_move");
+function blockPrint(frontBlock) {
+  currentBlock = frontBlock;
+  const beforeBtn = document.querySelector(".before_move");
+  const nextBtn = document.querySelector(".next_move");
 
-//   if (frontBlock <= 1) {
-//     beforeBtn.style.visibility = "hidden";
-//   } else {
-//     beforeBtn.style.visibility = "visible";
-//   }
+  if (frontBlock <= 1) {
+    beforeBtn.style.visibility = "hidden";
+  } else {
+    beforeBtn.style.visibility = "visible";
+  }
 
-// if (frontBlock + blockNum >= totalBlock) {
-//   nextBtn.style.visibility = "hidden";
-// } else {
-//   nextBtn.style.visibility = "visible";
-// }
+  if (frontBlock + blockNum >= totalBlock) {
+    nextBtn.style.visibility = "hidden";
+  } else {
+    nextBtn.style.visibility = "visible";
+  }
 
-// let blockBox = document.querySelector(".block");
-// blockBox.replaceChildren();
+  let blockBox = document.querySelector(".block");
+  blockBox.replaceChildren();
 
-// console.log("remove");
+  console.log("remove");
 
-//front_block부터 total_block 또는 block_num까지 생성 및 추가
-//   for (let i = frontBlock; i <= totalBlock && i < frontBlock + blockNum; i++) {
-//     console.log("add element");
+  // front_block부터 total_block 또는 block_num까지 생성 및 추가
+  for (let i = frontBlock; i <= totalBlock && i < frontBlock + blockNum; i++) {
+    console.log("add element");
 
-//     // 버튼을 생성한다.s
-//     let pageButton = document.createElement("button");
-//     pageButton.textContent = i;
-//     // 버튼을 클릭하면 게시글이 변경되는 이벤트 추가
-//     pageButton.addEventListener("click", function (event) {
-//       pagePrint(i);
-//     });
-//     // 블럭에 추가한다.
-//     blockBox.appendChild(pageButton);
-//   }
-// }
+    // 버튼을 생성한다.s
+    let pageButton = document.createElement("button");
+    pageButton.textContent = i;
+    // 버튼을 클릭하면 게시글이 변경되는 이벤트 추가
+    pageButton.addEventListener("click", function (event) {
+      pagePrint(i);
+    });
+    // 블럭에 추가한다.
+    blockBox.appendChild(pageButton);
+  }
+}
 
-// function before() {
-//   blockPrint(currentBlock - blockNum);
-//   console.log("이전");
-// }
+function before() {
+  blockPrint(currentBlock - blockNum);
+  console.log("이전");
+}
 
-// function next() {
-//   blockPrint(currentBlock + blockNum);
-//   console.log("다음");
-// }
-// // 화면 로드 시 실행되는 이벤트
-// window.onload = function () {
-//   dataPrint(1);
-//   blockPrint(1);
-// };
+function next() {
+  blockPrint(currentBlock + blockNum);
+  console.log("다음");
+}
+// 화면 로드 시 실행되는 이벤트
+window.onload = function () {
+  sliceBoardsStr(1);
+
+  blockPrint(1);
+};
