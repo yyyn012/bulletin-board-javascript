056afe9653a51cd850030690dc3cde4a2ee202c3
yyyn012 <111561826+yyyn012@users.noreply.github.com>
2024-05-31 21:55:04 +0900

    organizing

--- js/list.js
+++ js/list.js
@@ -23,10 +23,9 @@ for (let i = 0; i < boardsObj.length; i++) {
 
 // pagination 변수
 let pageLength = totalPage.length;
-let pageNum = 10;
-let blockNum = 5;
+const pageNum = 5;
+const blockNum = 5;
 let totalBlock = Math.ceil(pageLength / pageNum);
-let page = 1;
 
 // 데이터 출력 함수
 const template = (index, objValue) => {
@@ -63,14 +62,14 @@ const mouseOut = (event) => {
   event.target.style.fontWeight = "500";
 };
 
-// 버튼에 따라 데이터 나눠서 보여주기
+// 버튼에 따라 데이터 나눠서 보여주는 함수
 const sliceDataPrint = (block) => {
   // 리스트 초기화 ( tbody.hasChildNodes의 값이 유동적이므로 while문 사용)
   while (tbody.hasChildNodes()) {
-    tbody.removeChild();
+    tbody.removeChild(tbody.lastChild);
   }
 
-  // 화면에 pageNum의 값만큼 글 생성
+  // 화면에 pageNum만큼씩의 글 생성
   let start = pageLength - pageNum * (block - 1);
 
   for (let i = start; i >= 1 && i > start - pageNum; i--) {
@@ -82,7 +81,9 @@ const sliceDataPrint = (block) => {
 };
 
 // 버튼 출력
-function blockPrint(frontBlock) {
+let page = 1;
+
+const blockPrint = (frontBlock) => {
   page = frontBlock;
   const beforeBtn = document.querySelector(".before_move");
   const nextBtn = document.querySelector(".next_move");
@@ -115,7 +116,7 @@ function blockPrint(frontBlock) {
 
     blockBox.appendChild(pageButton);
   }
-}
+};
 
 // before 버튼, next 버튼
 function before() {
