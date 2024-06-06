d475f56067c7761ff92b03a0993e2afd7d71296f
yyyn012 <111561826+yyyn012@users.noreply.github.com>
2024-06-06 21:59:06 +0900

    pagination practice 2

--- js/list.js
+++ js/list.js
@@ -3,8 +3,6 @@
 // 데이터 가져오기
 const BOARDS = "boards";
 const boardsStr = localStorage.getItem(BOARDS);
-const boardsObj = JSON.parse(localStorage.getItem(BOARDS));
-const noticeBoard = document.querySelector(".notice_board");
 
 // boards 초기값 지정
 if (boardsStr === null) {
@@ -15,42 +13,40 @@ if (boardsStr === null) {
 
 // pagination
 
-// 데이터 배열로 저장하기
+// pagination 변수 설정
+const boardsObj = JSON.parse(localStorage.getItem(BOARDS));
+
 let totalPage = [];
 for (let i = 0; i < boardsObj.length; i++) {
   totalPage.push(boardsObj[i]);
 }
-// pagination 변수
-let pageLength = totalPage.length;
-const pageNum = 5;
-const blockNum = 5;
-let totalBlock = Math.ceil(pageLength / pageNum);
 
-// 데이터 출력 함수
+let pageLength = totalPage.length;
+let pageNum = 5;
+let blockNum = 5;
+let totalBlock = Math.ceil(pageLength / pageNum);
+const noticeBoard = document.querySelector(".notice_board");
+
+// template 생성
 const template = (index, objValue) => {
   return `
-    <tr>
-      <td>${index + 1}</td>
-      <td>
-        <a href="/board/view.html?index=${objValue.index}"
-          onmouseover={mouseOver(event)}
-          onmouseout={mouseOut(event)}
-          style="
-          display:inline-block;
-          width:90px;
-          "
-        >
-          ${objValue.subject}
-        </a>
-      </td>
-      <td>${objValue.writer}</td>
-      <td>${objValue.date}</td>
-      <td>${objValue.views}</td>
-    </tr>
-  `;
+        <tr>
+            <td>${index + 1}</td>
+            <td>
+                <a onmouseover={mouseOver(event)}
+                    onmouseout={mouseOut(event)}
+                    href="/board/view.html?index=${objValue.index}"
+                    style="display:inline-block; width:90px;"
+                >${objValue.subject}
+                </a>
+            </td>
+            <td>${objValue.writer}</td>
+            <td>${objValue.date}</td>
+            <td>${objValue.views}</td>
+        </tr>
+    `;
 };
 
-// mouseover 시 글자 색 / 굵기 변경
 const mouseOver = (event) => {
   event.target.style.color = "rgb(167, 215, 215)";
   event.target.style.fontWeight = "700";
@@ -61,14 +57,12 @@ const mouseOut = (event) => {
   event.target.style.fontWeight = "500";
 };
 
-// 버튼에 따라 데이터 나눠서 보여주는 함수
+// 버튼에 따라 데이터 나눠서 보여주기
 const sliceDataPrint = (block) => {
-  // 리스트 초기화 ( noticeBoard.hasChildNodes의 값이 유동적이므로 while문 사용)
   while (noticeBoard.hasChildNodes()) {
     noticeBoard.removeChild(noticeBoard.lastChild);
   }
 
-  // 화면에 pageNum만큼씩의 글 최근 순으로 생성
   let start = pageLength - pageNum * (block - 1);
 
   for (let i = start; i >= 1 && i > start - pageNum; i--) {
@@ -81,6 +75,7 @@ const sliceDataPrint = (block) => {
 
 // 버튼 출력
 let page = 1;
+
 const blockPrint = (frontBlock) => {
   page = frontBlock;
   const beforeBtn = document.querySelector(".before_btn");
@@ -103,10 +98,9 @@ const blockPrint = (frontBlock) => {
   let blockBox = document.querySelector(".block");
   blockBox.replaceChildren();
 
-  // frontBlock부터 버튼 생성 및 추가하기
+  //  frontBlock부터 버튼 생성 및 추가하기
   for (let i = frontBlock; i <= totalBlock && i < frontBlock + blockNum; i++) {
-    // 버튼 생성
-    let pageButton = document.createElement("button");
+    const pageButton = document.createElement("button");
     pageButton.textContent = i;
 
     // 버튼 클릭 시 게시글 변경
@@ -118,7 +112,7 @@ const blockPrint = (frontBlock) => {
   }
 };
 
-// before 버튼, next 버튼
+// beforeBtn, nextBtn
 const before = () => {
   blockPrint(page - blockNum);
 };
