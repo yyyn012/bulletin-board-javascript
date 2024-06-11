0ae4d92d797915b649bdf56708d0c4c048193649
yyyn012 <111561826+yyyn012@users.noreply.github.com>
2024-06-11 16:50:28 +0900

    code organize

--- js/list.js
+++ js/list.js
@@ -1,4 +1,4 @@
-// 데이터 가져오기, boards초기값 지정, pagination(데이터 배열로 저장하고 변수 지정, 템플릿 생성, 마우스 오버 시 제목 글자 색 변경, 버튼에 따라 데이터 나눠서 보여주기, 버튼 출력, before 버튼/next 버튼, 새로고침 시 최근 글부터 보여주기)
+// 데이터 가져오기, boards초기값 지정, pagination(데이터 배열로 저장하고 변수 지정 템플릿 생성, 마우스 오버 시 제목 글자 색 변경, 버튼에 따라 데이터 나눠서 보여주기, 버튼 출력, before 버튼/next 버튼, 새로고침 시 최근 글부터 보여주기)
 
 // 데이터 가져오기
 const BOARDS = "boards";
@@ -13,7 +13,7 @@ if (boardsStr === null) {
 
 // pagination
 
-// 변수 지정
+// 데이터 배열로 저장하기
 const boardsObj = JSON.parse(localStorage.getItem(BOARDS));
 
 let totalPage = [];
@@ -21,34 +21,40 @@ for (let i = 0; i < boardsObj.length; i++) {
   totalPage.push(boardsObj[i]);
 }
 
+// pagination 변수 지정
+
 let pageLength = totalPage.length;
 let pageNum = 5;
 let blockNum = 5;
 let totalBlock = Math.ceil(pageLength / pageNum);
 
-// template
+// 데이터 출력 함수
 const template = (index, objValue) => {
   return `
-    <tr>
-      <td>
-        ${index + 1}
-      </td>
-      <td>
-      <a href="/board/view.html?index=${objValue.index}"
-        style="display:inline-block; width:90px;"
-        onmouseover={mouseOver(event)}
-        onmouseout={mouseOut(event)}
-      >
-          ${objValue.subject}
-        </a>
-      </td>
-      <td>${objValue.writer}</td>
-      <td>${objValue.date}</td>
-      <td>${objValue.views}</td>
-    </tr>
-  `;
+      <tr>
+        <td>
+          ${index + 1}
+        </td>
+        <td>
+        <a href="/board/view.html?index=${objValue.index}"
+          style="
+          display:inline-block;
+          width:90px;
+          "
+          onmouseover={mouseOver(event)}
+          onmouseout={mouseOut(event)}
+        >
+            ${objValue.subject}
+          </a>
+        </td>
+        <td>${objValue.writer}</td>
+        <td>${objValue.date}</td>
+        <td>${objValue.views}</td>
+        </tr>
+      `;
 };
 
+// mouseover 시 글자 색 / 굵기 변경
 const mouseOver = (event) => {
   event.target.style.color = "rgb(167, 215, 215)";
   event.target.style.fontWeight = "700";
@@ -59,7 +65,7 @@ const mouseOut = (event) => {
   event.target.style.fontWeight = "500";
 };
 
-// 버튼에 따라 데이터 나눠서 보여주기
+// 버튼에 따라 데이터 나눠서 보여주는 함수
 const noticeBoard = document.querySelector(".notice_board");
 
 const sliceDataPrint = (block) => {
@@ -67,8 +73,10 @@ const sliceDataPrint = (block) => {
     noticeBoard.removeChild(noticeBoard.lastChild);
   }
 
+  // 화면에 pageNum만큼씩의 글 생성
   let startPage = pageLength - pageNum * (block - 1);
-  for (let i = startBlock; i >= 1 && i < startPage - pageNum; i--) {
+
+  for (let i = startPage; i >= 1 && i > startPage - pageNum; i--) {
     noticeBoard.innerHTML += template(i - 1, boardsObj[i - 1]);
     boardsObj[i - 1].refresh = false;
     const refreshStr = JSON.stringify(boardsObj);
@@ -78,6 +86,7 @@ const sliceDataPrint = (block) => {
 
 // 버튼 출력
 let page = 1;
+
 const blockPrint = (frontBlock) => {
   page = frontBlock;
   const beforeBtn = document.querySelector(".before_btn");
@@ -100,27 +109,32 @@ const blockPrint = (frontBlock) => {
   let blockBox = document.querySelector(".block");
   blockBox.replaceChildren();
 
+  // front_block부터 total_block 또는 block_num까지 버튼 생성 및 추가하기
+  // frontBlock부터 totalBlock 또는 block_num까지 버튼 생성 및 추가하기
   for (let i = frontBlock; i <= totalBlock && i < frontBlock + blockNum; i++) {
-    const pageButton = document.createElement("button");
+    // 버튼 생성
+    let pageButton = document.createElement("button");
     pageButton.textContent = i;
 
-    pageButton.addEventListener("click", function (e) {
+    // 버튼 클릭 시 게시글 변경
+    pageButton.addEventListener("click", function (event) {
       sliceDataPrint(i);
     });
+
     blockBox.appendChild(pageButton);
   }
 };
 
-// before 버튼 / next 버튼
-const before = () => {
+// before 버튼, next 버튼
+function before() {
   blockPrint(page - blockNum);
-};
+}
 
-const next = () => {
+function next() {
   blockPrint(page + blockNum);
-};
+}
 
-// 새로고침 시 최근 글 ( = 버튼이 1인 경우) 부터 보여주기
+// 새로고침 시 최근 글(버튼이 1인 경우)부터 보여주기
 window.onload = () => {
   sliceDataPrint(1);
   blockPrint(1);
